95262abe3cb9f86cc6b5eea878494d2f8e9fdfea
jonathansmurray <jonathansmurray@gmail.com>
2021-09-21 17:02:32 -0700

    commit tree 2a4a7fdc956462ac1f25ffe4096de273b8f24b09

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {
+    result = "This one is on me!"
+  } else if (someValue>2000 && someValue<2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = 'No can do.'
+  }
+
+ 
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity) {
+  return "NYC" == someCity ? 'Ok, sounds good.' :  'No go.'
 }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  } 
 }
